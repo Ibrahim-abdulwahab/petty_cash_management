0b42fa186e742aedbcbb0f37379d343671a854bf
Ibrahim Abdul Wahab Mohammed <ibrahim.abdulwahab1997@gmail.com>
2026-09-16 17:30:30 +0300

    naming for petty cash whish

--- center_expense_management/center_expense_management/doctype/petty_cash_whish/petty_cash_whish.py
+++ center_expense_management/center_expense_management/doctype/petty_cash_whish/petty_cash_whish.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2026, OmniSync and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class PettyCashWhish(Document):
-	pass
+
+    def before_insert(self):
+        if not self.month_and_year:
+            frappe.throw("Month and Year is required.")
+
+        month = self.month_and_year.month
+        year = self.month_and_year.year
+
+        self.name = f"PCW-{month:02d}-{year}"
